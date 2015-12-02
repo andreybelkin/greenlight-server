8beddeb682be7a39226bccd4d809a14951789e5f
Andrey Belkin <Andrew.Belkin@gmail.com>
2015-12-02 11:20:43 +0500

    added all tables

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -47,3 +47,54 @@ CREATE TABLE `greenlight`.`channel` (
   `channel_name` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`channel_id`));
 
+CREATE TABLE `greenlight`.`street_channel` (
+  `sc_id` INT NOT NULL AUTO_INCREMENT,
+  `street_id` INT NOT NULL,
+  `channel_id` INT NOT NULL,
+  PRIMARY KEY (`sc_id`));
+
+ALTER TABLE `greenlight`.`street_channel`
+ADD INDEX `fk_sc_street_idx` (`street_id` ASC),
+ADD INDEX `fk_sc_channel_idx` (`channel_id` ASC);
+ALTER TABLE `greenlight`.`street_channel`
+ADD CONSTRAINT `fk_sc_street`
+  FOREIGN KEY (`street_id`)
+  REFERENCES `greenlight`.`street` (`street_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_sc_channel`
+  FOREIGN KEY (`channel_id`)
+  REFERENCES `greenlight`.`channel` (`channel_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TABLE `greenlight`.`user_channel` (
+  `channel_id` INT NOT NULL AUTO_INCREMENT,
+  `channel_name` TEXT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`channel_id`));
+
+CREATE TABLE `greenlight`.`user_channel_street` (
+  `ucs_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `channel_id` INT NOT NULL,
+  PRIMARY KEY (`ucs_id`));
+
+ALTER TABLE `greenlight`.`user_channel_street`
+CHANGE COLUMN `user_id` `street_id` INT(11) NOT NULL ;
+
+ALTER TABLE `greenlight`.`user_channel_street`
+ADD INDEX `fk_ucs_channel_idx` (`channel_id` ASC),
+ADD INDEX `fk_ucs_street_idx` (`street_id` ASC);
+ALTER TABLE `greenlight`.`user_channel_street`
+ADD CONSTRAINT `fk_ucs_channel`
+  FOREIGN KEY (`channel_id`)
+  REFERENCES `greenlight`.`user_channel` (`channel_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_ucs_street`
+  FOREIGN KEY (`street_id`)
+  REFERENCES `greenlight`.`street` (`street_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
