ddbae41e496af664cbd73b73712b96ed258067e7
Andrey Belkin <Andrew.Belkin@gmail.com>
2015-12-02 11:05:55 +0500

    added two tables

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -1,2 +1,21 @@
 CREATE SCHEMA `greenlight` DEFAULT CHARACTER SET utf8 ;
 
+CREATE TABLE `greenlight`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`user_id`));
+
+CREATE TABLE `greenlight`.`comment` (
+  `comment_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `message` TEXT NOT NULL,
+  PRIMARY KEY (`comment_id`));
+
+ALTER TABLE `greenlight`.`comment`
+ADD INDEX `fk_comment_user_idx` (`user_id` ASC);
+ALTER TABLE `greenlight`.`comment`
+ADD CONSTRAINT `fk_comment_user`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `greenlight`.`user` (`user_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
