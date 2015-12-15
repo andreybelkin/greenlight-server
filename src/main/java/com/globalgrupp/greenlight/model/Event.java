ea35c6f45c13f89e82b87dabdb1f7e4f867422a0
unknown <poplygin@poplygin.prognoz.ru>
2015-12-15 23:58:26 +0500

    rest service через spring boot, классы для hibernate

--- src/main/java/com/globalgrupp/greenlight/model/Event.java
+++ src/main/java/com/globalgrupp/greenlight/model/Event.java
@@ -1,10 +1,65 @@
 package com.globalgrupp.greenlight.model;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Andrey Belkin
  * Date: 02.12.2015
  * Time: 11:21
  */
+@Entity
+@Table(name="event")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="event_id")
+    private Long id;
+
+    @Column(name="message")
+    private String message;
+
+    @ManyToOne()
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY,cascade=CascadeType.ALL,mappedBy = "event")
+    private Set<Comment> comments=new HashSet<Comment>(0);
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Event() {
+    }
 }
